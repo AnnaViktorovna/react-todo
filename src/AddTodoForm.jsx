1b4860a107c134d248e11d27cf3550ee64acb6ec
AnnaViktorovna <anna.bazileeva@gmail.com>
2024-07-03 00:33:05 -0400

    add new components, refactoring

--- src/AddTodoForm.jsx
+++ src/AddTodoForm.jsx
@@ -1,8 +1,25 @@
-function AddTodoForm() {
+import { useState } from "react";
+
+function AddTodoForm(props) {
+    const [todoTitle, setTodoTitle] = useState("");
+
+    function handleAddTodo(event) {
+        event.preventDefault();
+        console.log(todoTitle);
+        if (props.onAddTodo) {
+            props.onAddTodo(todoTitle);
+        }
+        setTodoTitle("");
+    }
+
     return (
-        <form>
+        <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Title</label>
-            <input id="todoTitle" />
+            <input
+                id="todoTitle"
+                value={todoTitle}
+                onChange={(el) => setTodoTitle(el.target.value)}
+            />
             <button type="submit">Add</button>
         </form>
     );
