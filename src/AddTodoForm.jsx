f82cf09a6e3c8141912a8bc74f16ae8957c67c83
AnnaViktorovna <anna.bazileeva@gmail.com>
2024-07-14 00:32:44 -0400

    using lift state, clear form from hard-code

---
+++ src/AddTodoForm.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function AddTodoForm({ onAddTodo }) {
+    const [todoTitle, setTodoTitle] = useState("");
+
+    function handleTitleChange(event) {
+        setTodoTitle(event.target.value);
+    }
+
+    function handleAddTodo(event) {
+        event.preventDefault();
+
+        console.log(todoTitle);
+        onAddTodo({ title: todoTitle, id: Date.now() });
+        setTodoTitle("");
+    }
+
+    return (
+        <form onSubmit={handleAddTodo}>
+            <label htmlFor="todoTitle">Title</label>
+            <input
+                id="todoTitle"
+                type="text"
+                name="title"
+                value={todoTitle}
+                onChange={handleTitleChange}
+            />
+            <button type="submit">Add</button>
+        </form>
+    );
+}
+
+export default AddTodoForm;
---
+++ src/TodoList.jsx
@@ -0,0 +1,13 @@
+import TodoListItem from "./TodoListItem";
+
+function TodoList({ todoList }) {
+    return (
+        <ul>
+            {todoList.map((todo) => (
+                <TodoListItem key={todo.id} item={todo} />
+            ))}
+        </ul>
+    );
+}
+
+export default TodoList;
