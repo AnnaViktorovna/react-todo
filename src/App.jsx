34708536600130afac15062e76bf0c7a557ff8fa
AnnaViktorovna <anna.bazileeva@gmail.com>
2024-07-23 01:36:56 -0400

    todoList saved in localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,21 @@ import { useState } from "react";
 import "./App.css";
 import AddTodoForm from "./AddTodoForm";
 import TodoList from "./TodoList";
+import { useEffect } from "react";
+
 
 function App() {
-    const [todoList, setTodoList] = useState([]);
+    const [todoList, setTodoList] = useState(
+        JSON.parse(localStorage.getItem("savedTodoList")) || []
+    );
 
     function addTodo(newTodo) {
         setTodoList((prevTodos) => [...prevTodos, newTodo]);
     }
+
+    useEffect(() => {
+        localStorage.setItem("savedTodoList", JSON.stringify(todoList));
+    }, [todoList]);
 
     return (
         <div className="App">
