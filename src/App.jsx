341c370878a8f3d86a37e083cb541a5a90f1f3d1
AnnaViktorovna <anna.bazileeva@gmail.com>
2024-10-14 00:37:54 -0400

    Sort by Airtable view order

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
 
         const url = `https://api.airtable.com/v0/${
             import.meta.env.VITE_AIRTABLE_BASE_ID
-        }/${import.meta.env.VITE_TABLE_NAME}`;
+        }/${import.meta.env.VITE_TABLE_NAME}?view=Grid%20view&`;
 
         const options = {
             method: "GET",
